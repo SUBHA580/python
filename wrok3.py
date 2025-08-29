cfb4d95e4214d40c8faa8d5252c62e9432889233
SUBHA580 <rayanrafan4@gmail.com>
2025-08-29 12:07:49 +0600

    Add files via upload

---
+++ wrok3.py
@@ -0,0 +1,12 @@
+first_name= "Codingal"
+last_name="Educations"
+full_name= first_name+last_name
+example="Haa"*5
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("String Multiplied 5 times gives this result:", example)
+word="Coding"
+print("Length of string:", len(word))
+print("First letter of string:", word[0])
+print("String Sliced:", word[0:3])
