ea2d2ced9c0c81893d857217540826e44d4e9fb1
SUBHA580 <rayanrafan4@gmail.com>
2025-08-29 12:11:58 +0600

    Add files via upload

---
+++ work4.py
@@ -0,0 +1,10 @@
+x= input("Enter value of X:")
+y= input("Enter value of Y:")
+#swapping
+temp=x
+x=y
+y=temp
+
+#Displaying results after swapping
+print("value of x after swapping", x)
+print("value of y after swapping", y)
