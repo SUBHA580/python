f3bd2e7ff595c8ff9fda440e65dbf7d5eb0cf55c
SUBHA580 <rayanrafan4@gmail.com>
2025-08-29 12:26:44 +0600

    Add files via upload

---
+++ swapping  three numbers.py
@@ -0,0 +1,9 @@
+x= input("Enter any number you like:")
+y=input("Enter another number:")
+z=input("Enter the last number:")
+
+x, y, z = y, z, x
+print("value of x after swapping:", x)
+print("value of y after swapping:", y)
+print("value of z after swapping:", z)
+
