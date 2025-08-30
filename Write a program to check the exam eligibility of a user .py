9030fd48b34f1faa3be1a0c79ee689ab054f7a13
SUBHA580 <rayanrafan4@gmail.com>
2025-08-30 12:22:35 +0600

    Add files via upload

---
+++ Write a program to check the exam eligibility of a user .py
@@ -0,0 +1,13 @@
+mark=int(input("Enter your exam mark:"))
+if mark>=80:
+    print("You got A+")
+elif mark>=70:
+    print("You got A")
+elif mark>=60:
+    print("You got A-")
+elif mark>=50:
+    print("you got B")
+elif mark>=40:
+    print("You got B-")
+else:
+    print("you have failed")
