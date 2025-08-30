476d601329795959fa52e18a39b87dccf751dcd1
SUBHA580 <rayanrafan4@gmail.com>
2025-08-30 12:07:08 +0600

    Add files via upload

---
+++ Date time operators .py
@@ -0,0 +1,9 @@
+import datetime
+#using now() to get date time
+current_time=datetime.datetime.now()
+#printing value of now.
+print("Time now at greenwich meridian is: ", end="")
+print(current_time)
+#print calender of year 2021
+import calendar
+print("/n", calendar.calendar(2021))
